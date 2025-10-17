7aae938b42c0382a37bea1ebd0161751892a8726
NickCH-K <44816781+NickCH-K@users.noreply.github.com>
2025-10-16 23:55:25 -0700

    Comment out sessionInfo() in browse-CRAN.R
    Comment out sessionInfo() call for check validation

--- R/browse-CRAN.R
+++ R/browse-CRAN.R
@@ -71,7 +71,7 @@ table(causal_pkgs$word)
 # causal intervention       policy   propensity    treatment 
 #    226           49           53           82          491
 
-sessionInfo()
+# sessionInfo()
 # R version 3.5.1 (2018-07-02)
 # Platform: x86_64-apple-darwin15.6.0 (64-bit)
 # Running under: macOS  10.15.3
